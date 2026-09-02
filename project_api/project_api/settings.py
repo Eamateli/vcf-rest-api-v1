d018a796adc7e7fb024dc644c25d687129df153e
Eamateli <e.a.mateli@gmail.com>
2026-09-02 20:32:20 +0300

    Remove db from project and settings

--- project_api/project_api/settings.py
+++ project_api/project_api/settings.py
@@ -73,10 +73,7 @@ WSGI_APPLICATION = 'project_api.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    
 }
 
 
