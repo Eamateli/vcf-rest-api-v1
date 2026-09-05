e613f0540819191eb7d0cc356963cb7e18aca61a
Eamateli <e.a.mateli@gmail.com>
2026-09-05 18:55:04 +0300

    chore: move SECRET_KEY, DEBUG and ALLOWED_HOSTS to .env

--- config/settings.py
+++ config/settings.py
@@ -19,14 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^mjcgh*lqk9c&&klrc8mlpndshsv$zqd4s-2p7ml!f(07wekcp'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
